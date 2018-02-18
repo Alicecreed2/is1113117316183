afc1156606ee817052d4086214378506a1970e3c
Alice Creed <117316183@umail.ucc.ie>
2018-02-18 15:56:27 +0000

    project

--- ebusiness/ebus2.php
+++ ebusiness/ebus2.php
@@ -1,46 +1,75 @@
+<?php
+
+// Start the session
+
+session_start();
+
+?>
+
 <!DOCTYPE html>
+
 <html>
+
     <head>
 
-        <title> Enter Details</title>
+        <title>Enter Details</title>
 
+        
 
         <!--jQuery-->
+
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <script type="text/javascript" src="ebus2_validator.js"></script>
+
     </head>
 
     <body>
 
-        <h4>Please enter your payment details.</h4>
+        <h4>Please enter your payment details</h4>
 
-            <br />
+        
 
-            <form method = "POST" action = "Ebus3.php">
+        
 
-                <label for="user_pin">
-                     PIN
-                </label>
+            <form action="Ebus3.php" method="POST">
 
-                <input type="password" id="user_pin" placeholder="Card Pin" maxlength="4">
 
-                <button type="Submit" id="btnPurchase" disabled>
-                    Proceed with Purchase
-                </button>
+
+                    <label for="user_pin">PIN</label>
+
+                    
+
+                    <input type="password" id="user_pin" placeholder="Card PIN" maxlength="4">
+
+
+
+                <button type="submit" id="btnPurchase" disabled>Proceed with Purchase</button>
+
+              
 
             </form>
 
-            <br />
+            
 
-            <button onClick="validateDetails()"> Validate </button>
+            <br/>
 
-        <script type="text/javascript" src="ebus2_validator.js"></script>
+            <button onClick="validateDetails()">Validate</button>
 
+            
+
+            <?php
+
+            // Set session variables
+
+            $_SESSION["total"] = $_POST["total"];
+
+            ?>
+
+        
 
     </body>
 
-
-
 </html>
-
 
 
